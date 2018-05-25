a2d8af8660d8665f86752e2c02acacc8083f2bf5
lshep <shepherl@gmail.com>
2018-05-25 07:47:52 -0400

    Update for BiocCheck ERROR/NOTES
      - test files for checking BiocCheck
         T/F
         class() ==
         vignette validity
         set.seed
      - also added test for vignette unstated depenedncy check for R CMD check
         builder reports are not picking it up

--- R/test.R
+++ R/test.R
@@ -5,6 +5,34 @@ test <- function() {
 test2 <- function(){
 
     #x = c(10,20)
-    x = 20
+    x <- 20
     myHelper(x)
+}
+
+test3 <- function(){
+
+    mat1 <- matrix(1:20, nrow=5)
+    x <- list(orig = mat1, T = t(mat1))
+    x$T
+}
+
+test4 <- function(){
+    x <- T
+    x
+}
+
+test5 <- function(){
+    T <- "whatsinaname"
+    myX <- c(1:2)
+    names(myX) <- c( T , "rose")
+    myX[ T ] 
+}
+
+test6 <- function(){
+    x <- "alotoffunctioncode"
+    T
+}
+
+test7 <- function(){
+    set.seed(1)
 }
--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,11 @@
+Something <- "something"
+globalTruth <- T
+
 whoami <- function() {
     print("spbtest3!")
+}
+
+classtest <- function(){
+    vl = 1
+    class(vl)=="numeric"    
 }
